8abed56a6013237c10ea3733b688d47295792dec
axplay0829@gmail.com <axplay0829@gmail.com>
2014-11-12 05:37:33 -0500

    off

--- src/main/java/com/rodenbostel/sample/GreetingController.java
+++ src/main/java/com/rodenbostel/sample/GreetingController.java
@@ -3,7 +3,6 @@ package com.rodenbostel.sample;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GreetingController {
@@ -13,5 +12,4 @@ public class GreetingController {
 		// Thread.sleep(3000); // simulated delay
 		return new Greeting("Hello, " + message.getName() + "!");
 	}
-
 }
--- src/main/java/com/rodenbostel/sample/SampleController.java
+++ src/main/java/com/rodenbostel/sample/SampleController.java
@@ -1,24 +1,23 @@
 package com.rodenbostel.sample;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
+ 
 @RestController
+
 public class SampleController {
 
 	@Autowired
 	private CustomerRepository repository;
-
-	@RequestMapping("/customer")
-	public List<Customer> customer() {
-
-		repository.deleteAll();
+	
+    @RequestMapping("/customer")
+    public List<Customer> index() {
+    	
+    	repository.deleteAll();
 
 		// save a couple of customers
 		repository.save(new Customer("Alice", "Smith"));
@@ -45,29 +44,9 @@ public class SampleController {
 			System.out.println(customer);
 			customers.add(customer);
 		}
-
-		return customers;
-	}
-
-	@RequestMapping(value = "/timer", method = RequestMethod.GET)
-	public ModelAndView hello() {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("timer");
-
-		String str = "Hello World!";
-		mav.addObject("message", str);
-
-		return mav;
-	}
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index() {
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("index");
-
-		String str = "Hello World!";
-		mav.addObject("message", str);
-
-		return mav;
-	}
+		
+		
+        return customers;
+    }
+ 
 }
